7e37609a9ac78d853620be7e1d5f75fad63f0878
Ana A <aausek@users.noreply.github.com>
2021-06-22 19:15:09 -0400

    Created archived scripts directory

---
+++ archived scripts/geoparse.py
@@ -0,0 +1,42 @@
+import GEOparse, os, glob
+
+# Main() to collect gene name input or exit
+def main():
+    userInput = input('Enter gene name or ''exit'' to quit app: ')
+    if userInput == 'exit':
+        sys.exit()
+    else:
+        # Call parser function to query GEO database
+        parser(userInput)
+        #print("./output/" + userInput + "*")
+# Query GEO database for gene samples
+def parser(userInput):
+    
+    # User input used to query GEO and download to /output folder
+    gse = GEOparse.get_GEO(geo=userInput, destdir="./output")
+    
+    print("\nGSM example:")
+    
+    # Print metadata and table head for sample
+    for gsm_name, gsm in gse.gsms.items():
+        print("Name: ", gsm_name)
+        print("Metadata:",)
+        for key, value in gsm.metadata.items():
+            print(" - %s : %s" % (key, ", ".join(value)))
+        print ("Table data:",)
+        print (gsm.table.head())
+        break
+
+    # print("\nGPL example:")
+    # for gpl_name, gpl in gse.gpls.items():
+    #     print("Name: ", gpl_name)
+    #     print("Metadata:",)
+    #     for key, value in gpl.metadata.items():
+    #         print(" - %s : %s" % (key, ", ".join(value)))
+    #     print("Table data:",)
+    #     print(gpl.table.head())
+    #     break
+
+# Call main()
+if __name__ == '__main__':
+    main()
