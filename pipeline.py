6e235cb8b7c16d2d586d37f3b599ac8e6845f946
Ana A <aausek@users.noreply.github.com>
2021-07-19 11:23:51 -0400

    Refactoring inputMenu function with quit statement

--- pipeline.py
+++ pipeline.py
@@ -15,8 +15,8 @@ def banner():
     print('''
          ██████╗ ███████╗ ██████╗         ██████╗ ██╗██████╗ ███████╗██╗     ██╗███╗   ██╗███████╗
         ██╔════╝ ██╔════╝██╔═══██╗        ██╔══██╗██║██╔══██╗██╔════╝██║     ██║████╗  ██║██╔════╝
-        ██║  ███╗█████╗  ██║   ██║        ██████╔╝██║██████╔╝█████╗  ██║     ██║██╔██╗ ██║█████╗  
-        ██║   ██║██╔══╝  ██║   ██║        ██╔═══╝ ██║██╔═══╝ ██╔══╝  ██║     ██║██║╚██╗██║██╔══╝  
+        ██║  ███╗█████╗  ██║   ██║        ██████╔╝██║██████╔╝█████╗  ██║     ██║██╔██╗ ██║█████╗
+        ██║   ██║██╔══╝  ██║   ██║        ██╔═══╝ ██║██╔═══╝ ██╔══╝  ██║     ██║██║╚██╗██║██╔══╝
         ╚██████╔╝███████╗╚██████╔╝███████╗██║     ██║██║     ███████╗███████╗██║██║ ╚████║███████╗
          ╚═════╝ ╚══════╝ ╚═════╝ ╚══════╝╚═╝     ╚═╝╚═╝     ╚══════╝╚══════╝╚═╝╚═╝  ╚═══╝╚══════╝
     ''')
@@ -28,24 +28,27 @@ def inputMenu():
     while True:
         print('For full sample data download enter accession series code, and type FULL as query type. \n'
               'Type CUSTOM to specify custom query parameters.')
+
+         # Add cases to test that these inputs are not blank
+        acc = input('Enter acc component (gplxxx, gsmxxx or gsexxx): ')
+        query_type = input('Enter F for full or C for custom query type: ')
+        if query_type.lower() == 'f':
+            targ = 'gsm'
+            view = 'data'
+            form = 'text'
+            pullUrl(acc, targ, view, form)
+        elif query_type.lower() == 'c':
+            targ = input('Enter targ component value (self, gsm, gpl, gse or all): ')
+            view = input('Enter view component (brief, quick, data or full): ')
+            form = input('Enter form component (text, html or xml): ')
+            pullUrl(acc, targ, view, form)
+
         quit_app = input('Quit? Y/N: ')
+
         if quit_app.lower() == 'y':
             break
         elif quit_app.lower() == 'n':
-
-            # Add cases to test that these inputs are not blank
-            acc = input('Enter acc component (gplxxx, gsmxxx or gsexxx): ')
-            query_type = input('Enter FULL or CUSTOM query type: ')
-            if query_type.lower() == 'full':
-                targ = 'gsm'
-                view = 'data'
-                form = 'text'
-                pullUrl(acc, targ, view, form)
-            elif query_type.lower() == 'custom':
-                targ = input('Enter targ component value (self, gsm, gpl, gse or all): ')
-                view = input('Enter view component (brief, quick, data or full): ')
-                form = input('Enter form component (text, html or xml): ')
-                pullUrl(acc, targ, view, form)
+            inputMenu()
         else:
             inputMenu()
 
@@ -58,10 +61,12 @@ def pullUrl(acc, targ, view, form):
     # GEO request URL
     root_url = 'https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?'
     # Construct request URL with input parameters
-    request = root_url + 'acc=' + acc + '&targ=' + targ + '&view=' + view + '&form=' + form
+    request = root_url + 'acc=' + acc + '&targ=' + \
+        targ + '&view=' + view + '&form=' + form
 
     self_targ = 'self'
-    gpl_request = root_url + 'acc=' + acc + '&targ=' + self_targ + '&view=brief' + '&form=text'
+    gpl_request = root_url + 'acc=' + acc + '&targ=' + \
+        self_targ + '&view=brief' + '&form=text'
     print(request)
     print(gpl_request)
 
@@ -84,7 +89,8 @@ def pullUrl(acc, targ, view, form):
         os.mkdir(subfolder)
 
     extension = '.txt'
-    sample_filename = acc + '_' + targ + '_' + view + '_' + form + '_' + date + extension
+    sample_filename = acc + '_' + targ + '_' + \
+        view + '_' + form + '_' + date + extension
 
     gpl_filename = acc + '_' + 'meta_' + date + extension
 
@@ -102,6 +108,7 @@ def pullUrl(acc, targ, view, form):
         splitFiles(sample_filepath, subfolder, extension)
         getGPL(gpl_filepath, subfolder, extension, root_url)
 
+
 def splitFiles(sample_filepath, subfolder, extension):
     with open(sample_filepath, mode="r") as original_file:
         reader = original_file.read()
@@ -113,7 +120,7 @@ def splitFiles(sample_filepath, subfolder, extension):
             sample_code = line.strip(token)
             print(sample_code)
             samples.append(sample_code)
-            # print(samples)    
+            # print(samples)
     # Specify sample names
     for i, part in enumerate(reader.split(token)[1:]):
         filename = subfolder + "/Sample_" + str(i) + extension
@@ -124,6 +131,8 @@ def splitFiles(sample_filepath, subfolder, extension):
     os.remove(sample_filepath)
 
 # Parse 'self' GPL file
+
+
 def getGPL(gpl_filepath, subfolder, extension, root_url):
     with open(gpl_filepath, mode="r") as original_file:
         reader = original_file.read()
@@ -133,15 +142,16 @@ def getGPL(gpl_filepath, subfolder, extension, root_url):
             if token in line:
                 gpl_code = line.strip(token)
                 print(gpl_code)
-        
-                request = root_url + 'acc=' + gpl_code + '&targ=gpl' + '&view=full' + '&form=text'
-                gpl_full = gpl_code + '_' + extension
+
+                request = root_url + 'acc=' + gpl_code + \
+                    '&targ=gpl' + '&view=full' + '&form=text'
+                gpl_full = gpl_code + extension
 
                 # File and path name
                 gpl_full_path = os.path.join(subfolder, gpl_full)
                 urllib.request.urlretrieve(request, gpl_full_path)
                 # time.sleep(10)
-        
+
         original_file.close()
         os.remove(gpl_filepath)
 
